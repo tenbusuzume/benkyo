610ba326612fd5e475097f49ee0a0ef1efcc6010
DCF-NOTE34\yamaguchi.saori <yamaguchi.saori@dcf.co.jp>
2017-03-28 14:36:20 +0900

    3/28 14:30

--- MyBootApp/src/main/java/com/tuyano/springboot/MsgData.java
+++ MyBootApp/src/main/java/com/tuyano/springboot/MsgData.java
@@ -1,19 +1,14 @@
 package com.tuyano.springboot;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "msgdata")
@@ -29,35 +24,14 @@ public class MsgData {
 	private String title;
 
 	@Column(nullable = false)
-	@NotEmpty
 	private String message;
 
 	@ManyToOne
 	private MyData mydata;
 
-	@OneToMany(cascade=CascadeType.ALL)
-	@Column(nullable = true)
-	private List<MsgData> msgdatas;
-
-	public List<MsgData> getMsgDatas(){
-		return msgdatas;
-	}
-
-	public void setMsgdatas(List<MsgData> msgdatas){
-		this.msgdatas = msgdatas;
-	}
-
 	public MsgData() {
 		super();
 		mydata = new MyData();
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public long getId() {
@@ -68,12 +42,24 @@ public class MsgData {
 		this.id = id;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
 	public String getMessage() {
 		return message;
 	}
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public MyData getMyData() {
+		return mydata;
 	}
 
 	public void setMyData(MyData mydata) {
